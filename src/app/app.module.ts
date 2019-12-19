99a1bfe50882201c40d08f2e3ea11fca685befb9
raviraj-kiwitech <ravi.dwivedi@kiwitech.com>
2019-12-20 01:23:00 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DemoMaterialModule } from './module/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    DemoMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
